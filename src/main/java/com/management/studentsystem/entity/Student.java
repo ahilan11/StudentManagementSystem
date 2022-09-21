1316a8c6a6e91528afe9f1645ec6642beb45f533
Ahilan <ahilanbus@gmail.com>
2022-09-21 13:04:38 +0200

    New changes to Student Repository class and Student object class

--- src/main/java/com/management/studentsystem/entity/Student.java
+++ src/main/java/com/management/studentsystem/entity/Student.java
@@ -12,4 +12,9 @@ public class Student {
     @GeneratedValue
     private long id;
 
+    private String name;
+    private String surname;
+    private String email;
+
+
 }
--- src/main/java/com/management/studentsystem/repository/StudentRepository.java
+++ src/main/java/com/management/studentsystem/repository/StudentRepository.java
@@ -1,6 +1,7 @@
 package com.management.studentsystem.repository;
 
+import com.management.studentsystem.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface StudentRepository extends JpaRepository<Student,long> {
+public interface StudentRepository extends JpaRepository<Student,Long> {
 }
