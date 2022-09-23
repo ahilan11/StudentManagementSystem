48d994894353e781bfdec3fe09891ec4717b2909
Ahilan <ahilanbus@gmail.com>
2022-09-23 15:08:41 +0200

    finished Student Object and application.properties file

--- src/main/java/com/management/studentsystem/entity/Student.java
+++ src/main/java/com/management/studentsystem/entity/Student.java
@@ -3,18 +3,63 @@ package com.management.studentsystem.entity;
 import javax.persistence.*;
 
 @Entity
-@Table
+@Table(name = "student_database")
 public class Student {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
 
-    @Column(name = "student_name" , nullable= true)
+    @Column(name = "name" , nullable= true)
 
     private String name;
+    @Column(name = "surname" , nullable= true)
+
     private String surname;
+    @Column(name = "email" , nullable= true)
+
     private String email;
 
+    public Student(long id, String name, String surname, String email) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+    }
 
+    public Student(){
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
