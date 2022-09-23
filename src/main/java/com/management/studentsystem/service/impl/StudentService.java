3440bc07a7855cb775506d4aa28a485ff629e709
Ahilan <ahilanbus@gmail.com>
2022-09-23 14:32:23 +0200

    renamed StudentService to StudentServiceImp

--- src/main/java/com/management/studentsystem/service/impl/StudentService.java
+++ src/main/java/com/management/studentsystem/service/impl/StudentService.java
@@ -1,16 +1,20 @@
-package com.management.studentsystem.service;
+package com.management.studentsystem.service.impl;
 
 import com.management.studentsystem.entity.Student;
 import com.management.studentsystem.repository.StudentRepository;
 
-public class StudentService {
+public class StudentServiceImp {
     StudentRepository studentRepository;
 
-    public StudentService(StudentRepository studentRepository){
+    public StudentServiceImp(StudentRepository studentRepository){
         this.studentRepository = studentRepository;
     }
 
     public Student addStudent(Student student){
         return studentRepository.save(student);
+    }
+
+    public void deleteStudent(long id){
+        studentRepository.findById(id).
     }
 }
